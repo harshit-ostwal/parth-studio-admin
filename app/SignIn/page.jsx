2c4ec9ee48106d366916d72348374823d27a1f78
Harshit Jain <harshitostwal1234@gmail.com>
2024-01-06 13:10:16 +0530

    Parth Studio Admin

--- app/SignIn/page.jsx
+++ app/SignIn/page.jsx
@@ -1,18 +1,7 @@
 import SignIn from '@/components/SignIn'
-import { redirect } from 'next/navigation'
 import React from 'react'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export default async function page() {
-
-    //Getting The SignIn Session
-    const session = await getServerSession(authOptions);
-
-    //Checking SignIn Or Not
-    if (session) {
-        redirect("/");
-    }
 
     return (
         <>
