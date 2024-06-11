f8c331a57489bdd688ae7bda345753c0de2cc5db
Harshit Jain <harshitostwal1234@gmail.com>
2024-06-11 16:52:13 +0530

    Updated Fonts & Added Logo Images To Public Folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
-import { Inter } from "next/font/google";
+import { Onest } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const onest = Onest({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  preload: true,
+  variable: "--onest",
+  style: ["normal"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${onest.variable} font-Onest`}>
+        {children}
+      </body>
     </html>
   );
 }
